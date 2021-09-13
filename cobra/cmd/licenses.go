3d38f52ce7471b113e6ca4fd564711ae083f1c3e
cdoern <cdoern@redhat.com>
2021-09-13 08:49:47 -0400

    mod changes
    Signed-off-by: cdoern <cdoern@redhat.com>

--- cobra/cmd/licenses.go
+++ cobra/cmd/licenses.go
@@ -20,7 +20,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/spf13/cobra"
+	"github.com/cdoern/cobra"
 	"github.com/spf13/viper"
 )
 
--- doc/man_examples_test.go
+++ doc/man_examples_test.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"fmt"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
+	"github.com/cdoern/cobra"
+	"github.com/cdoern/cobra/doc"
 )
 
 func ExampleGenManTree() {
